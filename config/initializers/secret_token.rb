d05e80367b39945b5005353d28d086a7c7e985f4
Apptitud <apptitud@apptitud-3-desktop.(none)>
2013-02-27 14:37:07 -0500

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Appetite::Application.config.secret_token = 'e1b8e836e5cfab2499b3847d335a8cec918e4f5db64559430bf182cb3e13c11fa6485e4462480bb11ade7de3ffeae6962c783c2050997380bab586ddffc0c4ca'
